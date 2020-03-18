7b80eb7e54202b61b1adae10db2530836c30ec80
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-18 22:27:29 +0100

    add populate func to empoyees routes

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -14,7 +14,9 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const employee = await Employee.findOne().skip(rand);
+    const employee = await await Employee.findOne()
+      .populate('department')
+      .skip(rand);
     if (!employee) res.status(404).json({ message: 'Not found' });
     else res.json(employee);
   } catch (err) {
@@ -24,7 +26,9 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const employee = await Employee.findById(req.params.id);
+    const employee = await (await Employee.findById(req.params.id)).populated(
+      'department'
+    );
     if (!employee) {
       res.status(500).json({ message: 'Not found' });
     } else res.json(employee);
